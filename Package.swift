d9d5864b41931bc4e92985e339876308b2187733
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2021-09-12 13:32:15 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Config",
-    platforms: [.iOS(.v8), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2)],
+    platforms: [.iOS(.v10), .macOS(.v10_10)],
     products: [.library(name: "Config", targets: ["Config"])],
     dependencies: [],
     targets: [.target(name: "Config")]
